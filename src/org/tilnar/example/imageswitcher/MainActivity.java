247e6a2992e3b27c3b6ee986ececba25d13640e7
Aldryd <aldryd@tilnar.org>
2012-12-16 00:53:22 -0600

    Initial commit

---
+++ src/org/tilnar/example/imageswitcher/MainActivity.java
@@ -0,0 +1,31 @@
+package org.tilnar.example.imageswitcher;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ViewSwitcher;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Let the ViewSwitcher do the animation listening for you
+        ((ViewSwitcher) findViewById(R.id.switcher)).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                ViewSwitcher switcher = (ViewSwitcher) v;
+
+                if (switcher.getDisplayedChild() == 0) {
+                    switcher.showNext();
+                } else {
+                    switcher.showPrevious();
+                }
+            }
+        });
+    }
+
+}
